38db3f93fbaac2d62709c95389706ebd7323eaac
jsmith5 <jsmith5@EVITN-L200906>
2019-07-31 15:52:13 -0500

    Add project files.

---
+++ AddTwoNumbers/AddTwoNumbers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        //Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public partial class Solution
+        {
+            public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+            {
+                //Loop through each list prepending each number in the list to a string
+                var sum = (ConnectNumber(l1) + ConnectNumber(l2)).ToString();
+
+                //Initialize master node with value at the end of sum
+                ListNode SummedNumberListNode = new ListNode(sum[sum.Length-1]);
+
+                if(sum.Length > 1)
+                {
+                    SummedNumberListNode.next = new ListNode(sum[sum.Length - 2]);
+                    var workingNode = SummedNumberListNode;
+
+                    for(int i = sum.Length - 2; i > 0; i--)
+                    {
+                        var previousNode = workingNode;
+                        workingNode.next = new ListNode(sum[i]);
+
+                    }
+                }
+
+                return SummedNumberListNode;
+            }
+
+            public int ConnectNumber(ListNode numberList)
+            {
+                string number = "";
+                while (numberList.next != null)
+                {
+                    number = numberList.val.ToString() + number.ToString();
+                }
+                return int.Parse(number);
+            }
+        }
+    }
+}
---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        static void Main()
+        {
+            int[] test = { 1, 1,2, 7, 9 };
+            int[] indices = TwoSum(test, 9);
+
+            foreach(var value in indices)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+
+        static public int[] TwoSum(int[] nums, int target)
+        {
+            int[] indices = null;
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    sum = nums[i] + nums[j];
+                    if (sum == target)
+                    {
+                        indices = new int[] { i, j };
+                    }
+                }
+            }
+            return indices;
+        }
+    }
+}
