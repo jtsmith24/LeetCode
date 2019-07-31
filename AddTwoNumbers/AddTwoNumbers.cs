acd1a9c83c005b011ec57b8347df290c5e3581ec
jsmith5 <jsmith5@EVITN-L200906>
2019-07-31 17:07:26 -0500

    Passed initial test.

--- AddTwoNumbers/AddTwoNumbers.cs
+++ AddTwoNumbers/AddTwoNumbers.cs
@@ -13,38 +13,59 @@ namespace AddTwoNumbers
             public ListNode(int x) { val = x; }
         }
 
-        public partial class Solution
+        public class Solution
         {
-            public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+            static void Main()
+            {
+                ListNode oneMaster = new ListNode(2);
+                ListNode two = new ListNode(4);
+                ListNode three = new ListNode(3);
+                oneMaster.next = two;
+                two.next = three;
+
+                ListNode twoMaster = new ListNode(5);
+                ListNode four = new ListNode(6);
+                ListNode five = new ListNode(4);
+                twoMaster.next = four; four.next = five;
+
+                var addedListNode = AddTwoNumbers(oneMaster, twoMaster);
+            }
+
+            public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
             {
                 //Loop through each list prepending each number in the list to a string
                 var sum = (ConnectNumber(l1) + ConnectNumber(l2)).ToString();
+                var sumLength = sum.Length - 1;
 
                 //Initialize master node with value at the end of sum
-                ListNode SummedNumberListNode = new ListNode(sum[sum.Length-1]);
+                ListNode initialNode = new ListNode(int.Parse(sum[sumLength].ToString()));
 
-                if(sum.Length > 1)
+                //sum = 123
+                if (sum.Length > 1)
                 {
-                    SummedNumberListNode.next = new ListNode(sum[sum.Length - 2]);
-                    var workingNode = SummedNumberListNode;
+                    initialNode.next = new ListNode(int.Parse(sum[sumLength-1].ToString()));
+                    ListNode workingNode = initialNode.next;
 
-                    for(int i = sum.Length - 2; i > 0; i--)
+                    for (int i = sumLength - 2; i >= 0; i--)
                     {
                         var previousNode = workingNode;
-                        workingNode.next = new ListNode(sum[i]);
-
+                        workingNode = new ListNode(int.Parse(sum[i].ToString()));
+                        previousNode.next = workingNode;
                     }
                 }
 
-                return SummedNumberListNode;
+                return initialNode;
             }
 
-            public int ConnectNumber(ListNode numberList)
+            public static int ConnectNumber(ListNode numberList)
             {
                 string number = "";
-                while (numberList.next != null)
+                var nextNumber = numberList;
+
+                while (nextNumber != null)
                 {
-                    number = numberList.val.ToString() + number.ToString();
+                    number = nextNumber.val.ToString() + number.ToString();
+                    nextNumber = nextNumber.next;
                 }
                 return int.Parse(number);
             }
