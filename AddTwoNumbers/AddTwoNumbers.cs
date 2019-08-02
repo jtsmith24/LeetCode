86bdc481dc3936dc0eb3a4e87ab82ab8aaf6c8c4
jsmith5 <jsmith5@EVITN-L200906>
2019-08-02 13:56:41 -0500

    Final solution with accepted submission.

--- AddTwoNumbers/AddTwoNumbers.cs
+++ AddTwoNumbers/AddTwoNumbers.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
 using System.Threading.Tasks;
 
 namespace AddTwoNumbers
 {
     class Program
     {
+        public static List<int> l1NumberList = new List<int>()
+        {
+           1
+        };
+        public static List<int> l2NumberList = new List<int>()
+        {
+           1
+        };
+
         //Definition for singly-linked list.
         public class ListNode
         {
@@ -17,47 +28,32 @@ namespace AddTwoNumbers
         {
             static void Main()
             {
-                ListNode oneMaster = new ListNode(2);
-                ListNode two = new ListNode(4);
-                ListNode three = new ListNode(3);
-                oneMaster.next = two;
-                two.next = three;
+                ListNode l1 = InstantiateListNode(l1NumberList);
+                ListNode l2 = InstantiateListNode(l2NumberList);
 
-                ListNode twoMaster = new ListNode(5);
-                ListNode four = new ListNode(6);
-                ListNode five = new ListNode(4);
-                twoMaster.next = four; four.next = five;
-
-                var addedListNode = AddTwoNumbers(oneMaster, twoMaster);
+                var addedListNode = AddTwoNumbers(l1, l2);
             }
 
             public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
-            {
-                //Loop through each list prepending each number in the list to a string
+            {//Loop through each list prepending each number in the list to a string
                 var sum = (ConnectNumber(l1) + ConnectNumber(l2)).ToString();
-                var sumLength = sum.Length - 1;
 
                 //Initialize master node with value at the end of sum
-                ListNode initialNode = new ListNode(int.Parse(sum[sumLength].ToString()));
+                ListNode head = new ListNode(int.Parse(sum[sum.Length-1].ToString()));
 
-                //sum = 123
+                //Starts at the second to last index of sum string
                 if (sum.Length > 1)
                 {
-                    initialNode.next = new ListNode(int.Parse(sum[sumLength-1].ToString()));
-                    ListNode workingNode = initialNode.next;
-
-                    for (int i = sumLength - 2; i >= 0; i--)
+                    for (int i = sum.Length - 2; i >= 0; i--)
                     {
-                        var previousNode = workingNode;
-                        workingNode = new ListNode(int.Parse(sum[i].ToString()));
-                        previousNode.next = workingNode;
+                        head = InsertLast(head, int.Parse(sum[i].ToString()));
                     }
                 }
 
-                return initialNode;
+                return head;
             }
 
-            public static int ConnectNumber(ListNode numberList)
+            public static BigInteger ConnectNumber(ListNode numberList)
             {
                 string number = "";
                 var nextNumber = numberList;
@@ -67,7 +63,45 @@ namespace AddTwoNumbers
                     number = nextNumber.val.ToString() + number.ToString();
                     nextNumber = nextNumber.next;
                 }
-                return int.Parse(number);
+                return BigInteger.Parse(number);
+            }
+
+            public static ListNode InstantiateListNode(List<int> numbers)
+            {
+                ListNode head = null;
+                
+                //InsertLast
+                foreach(var number in numbers)
+                {
+                    head = InsertLast(head, number);
+                }
+
+                return head;
+            }
+
+            public static ListNode InsertLast(ListNode list, int number)
+            {
+                ListNode newNode = new ListNode(number);
+                if (list == null)
+                {
+                    return newNode;
+                }
+                ListNode lastNode = GetLastNode(list);
+                lastNode.next = newNode;
+                return list;
+            }
+
+            private static ListNode GetLastNode(ListNode head)
+            {
+                //base case
+                if(head.next == null)
+                {
+                    return head;
+                }
+                else
+                {
+                    return GetLastNode(head.next);
+                }
             }
         }
     }
